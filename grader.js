6e70b6229dac8b9c00d5f91550b78c78fca3051b
ryanm-ac <ryanm-ac@server.fake>
2013-07-07 06:19:42 -0400

    Updated grader.js and checks.json

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +38,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtml = function(html) {
+    return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerioHtml(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,11 +66,29 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'URL to index.html')
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+   if (program.url){
+       rest.get(program.url).on('complete', function(result, response) { 
+           if (result instanceof Error) {
+             return console.log('Error: ' + util.format(response));
+           }
+           var checkJson = checkHtml(result, program.checks);
+           var outJson = JSON.stringify(checkJson, null, 4);
+           console.log(outJson);
+       });
+    }
+    else {
+       fs.readFile(program.file, 'utf8', function (err, data) {
+           if (err) {
+             return console.log(err);
+           }
+           var checkJson = checkHtml(data, program.checks);
+           var outJson = JSON.stringify(checkJson, null, 4);
+           console.log(outJson);
+       });
+    }	
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
